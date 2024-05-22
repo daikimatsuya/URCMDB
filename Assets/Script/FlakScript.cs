0df8954982cfa2a734c11db993b9248d61fbc80b
k022g0055@g.neec.ac.jp <k022g0055@g.neec.ac.jp>
2024-05-22 17:11:37 +0900

    モデル少しいじった

--- Assets/Script/FlakScript.cs
+++ Assets/Script/FlakScript.cs
@@ -4,10 +4,51 @@ using UnityEngine;
 
 public class FlakScript : MonoBehaviour
 {
+    Transform pos;
+
+    private Transform playerPos;
+
+    [SerializeField] private Transform body;
+    [SerializeField] private Transform barrel;
+
+
+    [SerializeField] private bool isCanReach;
+
+    private void FlakController()
+    {
+        Aim();
+    }
+
+    private void Aim()
+    {
+        if(playerPos != null)
+        {
+
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isCanReach = true;
+            playerPos = other.transform;
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            isCanReach = false;
+            playerPos = null;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isCanReach=false;      
+        pos=GetComponent<Transform>();
     }
 
     // Update is called once per frame
