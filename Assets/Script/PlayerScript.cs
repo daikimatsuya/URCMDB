81bbddfc365de8f51e5f9b4ec3ad02dd78f4870f
k022g0055 <k022g0055@g.neec.ac.jp>
2024-07-08 08:35:47 +0900

    WASD対応

--- Assets/Script/PlayerScript.cs
+++ Assets/Script/PlayerScript.cs
@@ -68,19 +68,19 @@ public class PlayerScript : MonoBehaviour
     {
         if (isControl)
         {
-            if (Input.GetKey(KeyCode.UpArrow))
+            if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) 
             {
                 rowling.x -= rowlingSpeedY;
             }
-            if (Input.GetKey(KeyCode.DownArrow))
+            if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
             {
                 rowling.x += rowlingSpeedY;
             }
-            if (Input.GetKey(KeyCode.LeftArrow))
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
             {
                 rowling.y -= rowlingSpeedX;
             }
-            if (Input.GetKey(KeyCode.RightArrow))
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
             {
                 rowling.y += rowlingSpeedX;
             }
