4af6fc8111b7d8cd6c25b0897bbe01e0a6eeafe0
k022g0055@g.neec.ac.jp <k022g0055@g.neec.ac.jp>
2024-07-23 11:36:34 +0900

    タイトルのイージング作成

---
+++ Assets/Script/TitleCamera.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class TitleCamera : MonoBehaviour
+{
+        [SerializeField] private Vector3 firstPos;
+    [SerializeField] private Vector3 movePos;
+    [SerializeField] private float moveTime;
+
+
+    [SerializeField] private bool testBool;
+
+    private TitleScript ts;
+
+    Transform tf;
+
+    private float time;
+    
+
+    private void TitleCameraController()
+    {
+
+    }
+    private void Move(bool isStageSelect)
+    {
+        Vector3 dis;
+
+        if (isStageSelect)
+        {
+            float x = 1 - Mathf.Pow(1 - (time / moveTime), 3);
+            if (time < moveTime)
+            {           
+                dis = movePos - firstPos;
+                tf.position = new Vector3(firstPos.x + (dis.x * x), firstPos.y + (dis.y * x), firstPos.z + (dis.z * x));
+                time++;
+            }
+            else
+            {
+                tf.position = movePos;
+            }                       
+        }
+        else
+        {
+            float x = Mathf.Pow((time / moveTime), 3);
+            if (time>0)
+            {
+                dis = movePos - firstPos;
+                tf.position = new Vector3(firstPos.x - (dis.x * x), firstPos.y + (dis.y * x), firstPos.z - (dis.z * x));
+                time--;
+            }
+            else
+            {
+                tf.position = firstPos;
+            }
+        }
+    }
+    private void Test()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(testBool)
+            {
+                testBool=false;
+
+            }
+            else
+            {
+                testBool = true;
+
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        ts=GameObject.FindWithTag("TitleManager").GetComponent<TitleScript>();
+        tf=GetComponent<Transform>();
+        moveTime = moveTime * 60;
+        firstPos = tf.position;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move(testBool);
+        Test();
+    }
+}
--- Assets/Script/TitleScript.cs
+++ Assets/Script/TitleScript.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class TitleScript : MonoBehaviour
 {
+    private Transform cameraPos;
 
+    [SerializeField] private bool isStageSelect;
+    [SerializeField] private int selectStageCount;
+
+    private void TitleController()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            InStageSelect();
+        }
+    }
+    private void InStageSelect()
+    {
+        isStageSelect = true;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        Application.targetFrameRate = 60;
+        TitleController();
     }
 
     // Update is called once per frame
