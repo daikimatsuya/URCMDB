253fb3779b82fb66b0ec5817bf0af66c99de9bd7
k022g0055 <k022g0055@g.neec.ac.jp>
2024-09-28 01:21:48 +0900

    当たり判定付けて動くようにした

--- Assets/Script/FunScript.cs
+++ Assets/Script/FunScript.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class FunScript : MonoBehaviour
 {
+    private Transform tf;
+
+    [SerializeField] float rotateSpeed;
+
+    private Vector3 rotation;
+
+    private void RotateFun()
+    {
+        rotation = new Vector3(rotation.x+rotateSpeed,rotation.y,rotation.z);
+        tf.localEulerAngles = rotation;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        tf = GetComponent<Transform>();
+        rotation = tf.localEulerAngles;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RotateFun();
     }
 }
