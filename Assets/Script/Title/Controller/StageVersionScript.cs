cbd4d7b48362b5458cc98bec332a4610401a446c
k022g0055 <k022g0055@g.neec.ac.jp>
2024-12-07 19:00:19 +0900

    ステージのサブタイトル機能多分できた

--- Assets/Script/Title/Controller/StageVersionScript.cs
+++ Assets/Script/Title/Controller/StageVersionScript.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class StageVersionScript : MonoBehaviour
 {
+    [SerializeField] private GameObject[] stage;
+
+    private StageSelectScript sss;
+    
+
+    private void ViewVersion()
+    {
+        if(sss.GetStageCount().x == 0)
+        {
+            stage[(int)sss.GetStageCount().y].SetActive(false);
+            stage[(int)sss.GetStageCount().x+1].SetActive(false);
+        }
+        else if (sss.GetStageCount().x == sss.GetStageCount().y)
+        {
+            stage[0].SetActive(false);
+            stage[(int)sss.GetStageCount().x-1].SetActive(false);
+        }
+        else
+        {
+            stage[(int)sss.GetStageCount().x + 1].SetActive(false);
+            stage[(int)sss.GetStageCount().x - 1].SetActive(false);
+        }
+        stage[(int)sss.GetStageCount().x].SetActive(true);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        sss=GameObject.FindWithTag("TitleManager").GetComponent<StageSelectScript>();
+        ViewVersion();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ViewVersion();
     }
 }
