f26a16f9a3705ecc5650ec4c8178bb14663155c4
k022g0055@g.neec.ac.jp <k022g0055@g.neec.ac.jp>
2025-01-07 16:01:49 +0900

    ショートカット切り替え用障害物作成

---
+++ Assets/Script/InGame/Gimmick/LidStoneScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LidStoneScript : MonoBehaviour
+{
+    [SerializeField] GameObject shortcut;
+    [SerializeField] GameObject detour;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            shortcut.SetActive(true);
+            detour.SetActive(false);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        shortcut.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
