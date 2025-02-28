9b1246c8c1c629205992f8cb9b1b45f11d6a278b
k022g0055@g.neec.ac.jp <k022g0055@g.neec.ac.jp>
2025-02-28 15:04:58 +0900

    追加アイテム用のエフェクトを管理するスクリプトを作る

---
+++ Assets/Script/InGame/UI/RedBoostEffectScript.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedBoostEffectScript : MonoBehaviour
+{
+    [SerializeField] GameObject[] effect;
+    public void RedBoostEffectController()
+    {
+
+    }
+    public void StartRedBoostEffect()
+    {
+
+    }
+
+}
