f1eb8aa3358a20be80eb77279c8ec0fcc678fd5b
k022g0055@g.neec.ac.jp <k022g0055@g.neec.ac.jp>
2025-03-10 16:53:48 +0900

    ちょっと調整

--- Assets/Script/InGame/Gimmick/ExplodeEffectScript.cs
+++ Assets/Script/InGame/Gimmick/ExplodeEffectScript.cs
@@ -17,6 +17,8 @@ public class ExplodeEffectScript : MonoBehaviour
     private  Renderer[] renderers;
     private float dissolve;
     private float edge;
+
+    //初期化
     public void StartExplodeEffect()
     {
 
@@ -27,17 +29,22 @@ public class ExplodeEffectScript : MonoBehaviour
         renderers =GetComponentsInChildren<Renderer>();
 
     }
+
+    //サイズを大きくする
     public void SizeUp()
     {
         expantionSpeed = 1 - (expantionBuff / (expantionTime * 60));
         expantionSpeed = (1 - (float)Math.Pow(1 - expantionSpeed, 5) )* maxSize;
         tf.localScale = new Vector3(expantionSpeed, expantionSpeed, expantionSpeed);
     }
+
+    //回転させる
     public void Rotation()
     {
         tf.localEulerAngles += new Vector3(0, rotSpeed, 0);
     }
 
+    //ディゾルブさせる
     public void Dissolve()
     {
         dissolve = 1-(expantionBuff / (expantionTime * 60));
@@ -47,6 +54,7 @@ public class ExplodeEffectScript : MonoBehaviour
             renderers[i].material.SetFloat("_Dissolve", dissolve);
         }
     }
+    //端っこの色も時間で変える
     public void Edge()
     {
         edge = 1 - (expantionBuff / (expantionTime * 60));
@@ -56,10 +64,14 @@ public class ExplodeEffectScript : MonoBehaviour
             renderers[i].material.SetFloat("_Threshold", edge);
         }
     }
+
+    //時間を管理する
     public bool CountDown()
     {
         return TimeCountScript.TimeCounter(ref expantionBuff);
     }
+
+    //削除する
     public void Break()
     {
         Destroy(this.gameObject);
@@ -71,5 +83,16 @@ public class ExplodeEffectScript : MonoBehaviour
         this.maxSize=maxSize;
     }
     
+    public void SetDissolve(float dissolve)
+    {
+        this.dissolve = dissolve;
+    }
+    public void SetOffset(Vector2 tilling,Vector2 offset)
+    {
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            renderers[i].material.SetVector("_MainTex_ST", new Vector4(tilling.x, tilling.y, offset.x, offset.y));
+        }
+    }
     #endregion
 }
